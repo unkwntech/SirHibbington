c856501784d6921a02927684add824c50e16f7a6
Arron Chapman <achapman@test.home.madoneismad.com>
2024-07-21 20:29:44 -0500

    updated to remove temp directory

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -10,10 +10,10 @@ module.exports = {
       user: "achapman",
       host: "192.168.1.203",
       path: "/home/achapman/projects/SirHibbington/",
-      repo: "git@github.com/unkwntech/SirHibbington",
+      repo: "git@github.com:unkwntech/sirhibbington.git",
       ref: "origin/main",
       key: "~/.ssh/id_ed25519",
-      "post-deploy": "npm i; pm2 reload ecosystem.config.js --env production",
+      "post-deploy": "npm i; pm2 reload ecosystem.config.js --env production;rm -rf /home/achapman/projects/SirHibbington/source",
     },
   },
 };
